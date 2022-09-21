3a4b388bbc78480df5d3beef6c987d700f177744
An Vo <107334075+truonganvo@users.noreply.github.com>
2022-09-21 15:45:53 +1000

    Initial commit

---
+++ Assets/Sunnyland/Script/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float  MovementSpeed = 1f;
+    public float JumpForce = 1f;
+
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        //movement with transition
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        //jumping
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Sunnyland/Script/Traps.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traps : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("Mission Fail");
+        }
+    }
+}
