e2200f3f91947191ed00d076971ad21da8f2a668
An Vo <107334075+truonganvo@users.noreply.github.com>
2022-10-11 15:49:25 +1100

    Character animation tuning - Jumping

--- Assets/Sunnyland/Script/PlayerMovement.cs
+++ Assets/Sunnyland/Script/PlayerMovement.cs
@@ -9,14 +9,23 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D _rigidbody;
     public Animator animator;
+    [SerializeField] AudioSource jumpingSound;
 
+    private enum MovementState { idle, running, jumping, falling}
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        jumpingSound = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        MovementState state;
         //movement with transition & animation
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
@@ -24,26 +33,34 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.localScale = new Vector2(-1, 1);
-            animator.SetBool("running", true);
+            state = MovementState.running;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.localScale = new Vector2(1, 1);
-            animator.SetBool("running", true);
+            state = MovementState.running;
         }
         else
         {
-            animator.SetBool("running", false);
+            state = MovementState.idle;
         }
 
-        //jumping
+        //jumping & animation
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            jumpingSound.Play();
+        }
+        if (_rigidbody.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (_rigidbody.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
         }
 
-        
-        
+        animator.SetInteger("state", (int)state);
     }
 
 }
